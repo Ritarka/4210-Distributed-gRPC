c99fe67c78c874a54d347c0c7416f6158c8b17b0
Patel, Aditi N <apatel768@gatech.edu>
2023-10-29 19:33:56 -0400

    Connected to vendor

--- src/store.cc
+++ src/store.cc
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <vector>
 
 // #include "absl/flags/flag.h"
 // #include "absl/flags/parse.h"
@@ -11,7 +12,8 @@
 #include <grpcpp/grpcpp.h>
 
 #include "threadpool.h"
-#include "../external/protos/store.grpc.pb.cc"
+#include "../external/protos/store.grpc.pb.h"
+#include "../external/protos/vendor.grpc.pb.h"
 
 
 using grpc::Server;
@@ -24,13 +26,19 @@ using grpc::Status;
 using store::Store;
 using store::ProductQuery;
 using store::ProductReply;
+using store::ProductInfo;
 
+//add the vendors definitions
+using vendor::Vendor;
+using vendor::BidQuery;
+using vendor::BidReply;
 // ABSL_FLAG(std::string, addr_path, "", "filepath for vendor addresses");
 // ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
 // ABSL_FLAG(int32_t, num_threads, 1, "Max number of threads in threadpool");
 
 std::string addr_path;
-int port;
+std::string ip_addr; // for command line
+int port; //parse ip_addr to get the port
 int num_threads;
 
 class StoreSrv final {
@@ -59,7 +67,11 @@ class StoreSrv final {
     std::cout << "Server listening on " << server_address << std::endl;
 
     // Proceed to the server's main loop.
+    // Add the threadpool threads here, each thread will call HandleRpcs
+    
     HandleRpcs();
+    
+    
   }
 
  private:
@@ -69,8 +81,8 @@ class StoreSrv final {
     // Take in the "service" instance (in this case representing an asynchronous
     // server) and the completion queue "cq" used for asynchronous communication
     // with the gRPC runtime.
-    CallData(Store::AsyncService* service, ServerCompletionQueue* cq)
-        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+    CallData(Store::AsyncService* service, ServerCompletionQueue* cq, const std::string& vendor_address)
+        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE), vendor_address_(vendor_address) {
       // Invoke the serving logic right away.
       Proceed();
     }
@@ -91,10 +103,29 @@ class StoreSrv final {
         // Spawn a new CallData instance to serve new clients while we process
         // the one for this CallData. The instance will deallocate itself as
         // part of its FINISH state.
-        new CallData(service_, cq_);
-
+        new CallData(service_, cq_, vendor_address_);
+	
         // The actual processing.
-        std::string prefix("Hello ");
+        //no thread support right now
+        //make asyn rpc call to vendors
+        BidQuery vendor_request; //create a request to vendors
+        BidReply vendor_reply; // create a response from vendors
+        
+        //grpc channel to vendor using vendor_address_
+        std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(vendor_address_, grpc::InsecureChannelCredentials());
+        std::unique_ptr<Vendor::Stub> vendor_stub = Vendor::NewStub(channel);
+        
+        //make the call to vendor
+        grpc::ClientContext vendor_context;
+        vendor_stub->getProductBid(&vendor_context, vendor_request, &vendor_reply);
+        
+        //process vendor response
+        //ProductReply reply; //reply_ defined in private
+        ProductInfo* product_info = reply_.add_products();
+        product_info->set_vendor_id(vendor_reply.vendor_id());
+        product_info->set_price(vendor_reply.price());
+        
+        //std::string prefix("Hello ");
         // reply_.set_message(prefix + request_.name());
 
         // And we are done! Let the gRPC runtime know we've finished, using the
@@ -127,6 +158,8 @@ class StoreSrv final {
 
     // The means to get back to the client.
     ServerAsyncResponseWriter<ProductReply> responder_;
+    //vendor address
+    std::string vendor_address_;
 
     // Let's implement a tiny state machine with the following states.
     enum CallStatus { CREATE, PROCESS, FINISH };
@@ -136,7 +169,13 @@ class StoreSrv final {
   // This can be run in multiple threads if needed.
   void HandleRpcs() {
     // Spawn a new CallData instance to serve new clients.
-    new CallData(&service_, cq_.get());
+    // ADD- reading vendor_address file
+    std::vector<std::string> vendor_addresses = {"localhost:50051", "localhost:50052", "localhost:50053", "localhost:50054", "localhost:50055"};
+    //create Calldata for each vendor
+    for(const std::string& vendor_address : vendor_addresses){
+    	new CallData(&service_, cq_.get(), vendor_address);
+    }
+    //new CallData(&service_, cq_.get());
     void* tag;  // uniquely identifies a request.
     bool ok;
     while (true) {
@@ -160,7 +199,15 @@ class StoreSrv final {
 int main(int argc, char** argv) {
   
   addr_path = argv[1];
-  port = atoi(argv[2]);
+  ip_addr = argv[2];
+  //find the last occurence of :
+  size_t colon_pos = ip_addr.find_last_of(':');
+  if(colon_pos != std::string::npos){
+  	//extract the port number
+  	std::string port_str = ip_addr.substr(colon_pos + 1);
+  	port = atoi(port_str.c_str());
+  	std::cout << "Port: " << port << std::endl;
+  }
   num_threads = atoi(argv[3]);
 
   StoreSrv store;
