5bd1640a123295944cdea86ff0af7e3531d69d38
Patel, Aditi N <apatel768@gatech.edu>
2023-11-01 17:42:26 -0400

    now using async client

--- src/threadpool.h
+++ src/threadpool.h
@@ -1,6 +1,29 @@
 #pragma once
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <condition_variable>
+#include <atomic>
 
-class threadpool {
+class threadpool{
+
+public:
+	threadpool(size_t numThreads);
+	~threadpool();
+	
+	//template <class F, class... Args>
+	void enqueue(std::function<void(void)> func);
+	void wait();
+private:
+	size_t numThreads_;
+	std::vector<std::thread> threads_;
+	std::queue<std::function<void()>> tasks_;
+	std::mutex queueMutex_;
+	std::condition_variable cv_;
+	std::atomic<bool> stop_;
+	
+	void workerThread();
 	
 };
 
